2e80934e91a0af43c4cb743cb3951ed3b20c0a0e
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-31 20:17:49 +0200

    [UPD] rotte

--- app.js
+++ app.js
@@ -7,7 +7,27 @@ const app = express();
 // definisco la porta dove deve rimanare in ascolto il server
 const port = 3000;
 
+// inserisco il middleware per i file statici
+app.use(express.static("public"));
+
 // definisco la rotta base
-app.get("/menu", (req, res) => {
+app.get("/", (req, res) => {
   console.log("Server del mio blog");
+});
+
+//definisco la rotta dei post
+app.get("/posts", (req, res) => {
+  const posts = [
+    {
+      titolo: "Welcome",
+      contenuto: "Benvenuto nel mio blog!",
+      image: "/imgs/welcome.jpg",
+      tags: ["welcome", "benvenuto", "welcome aboard", "benvenuto a bordo"]
+    },
+  ];
+});
+
+// dico al server di rimanere in ascolto sulla porta 3000
+app.listen(port, () => {
+  console.log(`Server della pizzeria in ascolto sulla porta ${port}`);
 });
